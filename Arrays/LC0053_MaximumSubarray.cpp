ecba7f497065f665f1cf02038bbffd6e124b41a0
JosElton <73elton@gmail.com>
2026-09-08 15:46:42 +0530

    Code that finds maximum subarray value with linear time complexity

---
+++ Arrays/LC0053_MaximumSubarray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+     int maxsum=INT_MIN, currsum=0;
+     for (int val: nums){
+        currsum+=val;
+        maxsum=max(currsum,maxsum);
+        if (currsum<0){
+            currsum=0;
+        }
+     }
+     return maxsum;
+    }};
